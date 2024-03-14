67712201250dbea4bd0f1f5e78e5c4374f6ba29a
Rafael G. Francisco <Rafaelgfra@hotmail.com>
2024-03-14 17:56:28 -0300

    inserção do metodo POST para validar as credenciais ao clicar no botão de login

--- backend/src/Routes.js
+++ backend/src/Routes.js
@@ -3,8 +3,6 @@ import LoginController from "./app/controllers/LoginController.js";
 
 const router = Router();
 
-router.get('/teste', LoginController.teste)
-
 router.post('/login', LoginController.store)
 
 export default router
--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import routes from './Routes.js'
+import cors from 'cors'
 
 const app = express()
 app.use(express.json())// indicar para o express ler body com JSON
+
+app.use(cors())
 
 app.use(routes)
 
